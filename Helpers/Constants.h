5944af58367e23008d298a8c4880fe880c6a06aa
Anna B <annabeever1@gmail.com>
2024-09-19 08:18:31 -0500

    Add more maps

--- Helpers/Constants.h
+++ Helpers/Constants.h
@@ -15,7 +15,9 @@ namespace constants {
 	Int_t mintGreen = TColor::GetColor("#00CE7C");
 	double vDrift = 156.267;
 
-	std::map<int, std::string> configMap{{0, "classic"}, {1, "multiwire"}, {2, "multihit"}, {3, "angle"}, {4, "wireAndAngle"}};
+	std::map<int, std::string> configMap{{0, "classic"}, {1, "multiwire"}, {2, "multihit"}, {3, "angle"}, {4, "wireAndAngle"}, {5, "octant"}};
+	std::map<int, std::string> octantName{{1, "NW Upper"}, {2, "NE Upper"}, {3, "SW Upper"}, {4, "SE Upper"}, {5, "NW Lower"}, {6, "NE Lower"}, {7, "SW Lower"}, {8, "SE Lower"}};
+	std::map<int, std::string> plotFitName{{0, "noFit"}, {1, "withFit"}};
 
 }
 
